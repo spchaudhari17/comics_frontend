2865c7f6c4e7a48669b02b4d8f0318dfa0c1c3dc
Vasheem-Ahmad <vasheemahmad@virtualemployee.com>
2025-09-12 19:24:13 +0530

    Worked on the About Us + Contact Us page multiple section layout design with responsive

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -20,7 +20,7 @@ export const ContactUs = () => {
             </section>
 
             {/* Contact Form Section */}
-            {/* <section className="contact-form-sec py-5">
+            <section className="contact-form-sec py-5">
                 <div className="container-xl my-md-4">
                     <div className="heading-wrapper text-center mx-auto mb-4 pb-2 pb-md-4" style={{ maxWidth: '600px' }}>
                         <div className="label-heading mb-2">Need Help?</div>
@@ -98,12 +98,12 @@ export const ContactUs = () => {
                         </Col>
                     </Row>
                 </div>
-            </section> */}
+            </section>
 
             {/* Location Map Section */}
-            {/* <section className="location-map" >
+            <section className="location-map" >
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24275.292909952877!2d-74.4614855175898!3d40.48828017138096!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c3c355f06a92a5%3A0x25925506798e230c!2sNew%20Brunswick%2C%20NJ%2C%20USA!5e0!3m2!1sen!2sin!4v1757600137845!5m2!1sen!2sin" className="w-100 d-block border" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" style={{ height: '50vh' }}></iframe>
-            </section> */}
+            </section>
         </div>
     )
 }
