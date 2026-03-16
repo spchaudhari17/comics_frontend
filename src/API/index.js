a282eafaa1942c179fe80bcfa185c43f22338259
ajinkyakridemy <ajinkya.kridemy@gmail.com>
2026-03-16 18:42:35 +0530

    cahninh the ip

--- src/API/index.js
+++ src/API/index.js
@@ -3,9 +3,9 @@ import axios from "axios";
 
 const API = axios.create({
   // baseURL: "http://16.171.146.122:5000/api", // local server
-  baseURL: "http://localhost:5000/api", // IP server
+  // baseURL: "http://localhost:5000/api", // IP server
   // baseURL: "http://13.51.85.89:5000/api", // IP server
-  // baseURL: "https://api.kridemy.com/api", // production server
+  baseURL: "https://api.kridemy.com/api", // production server
 });
 
 
