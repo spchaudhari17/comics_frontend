b19cfd7775b3a54fcc462fbfc87030b4ee26d5c5
ajinkyakridemy <ajinkya.kridemy@gmail.com>
2026-09-20 00:12:51 +0530

    chaning the ip

--- src/API/index.js
+++ src/API/index.js
@@ -2,10 +2,10 @@ import axios from "axios";
 
 
 const API = axios.create({
-  // baseURL: "http://16.171.146.122:5000/api", // local server
-  // baseURL: "http://localhost:5000/api", // IP server
-  // baseURL: "http://51.21.194.3:5000/api", // IP server for live
-  baseURL: "https://api.kridemy.com/api", // production server
+  // baseURL: "http://16.171.146.122:5000/api", // testing server IP
+  // baseURL: "http://localhost:5000/api", // local IP server
+  // baseURL: "http://51.21.194.3:5000/api", // live server IP
+  baseURL: "https://api.kridemy.com/api", // production server 
 });
 
 
